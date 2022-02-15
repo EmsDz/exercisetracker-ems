a39a285d00bc234ae53ba7f9d4b78c5fb327cfcc
EmsDz <Ediaz@intellisys.com.do>
2022-02-15 17:26:01 -0400

    Add:
    - .gitignore
    - Dependencies update
    - .env

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+// mongoose.connect(process.env.URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+app.use(cors());
+
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
