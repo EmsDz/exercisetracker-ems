19831ad3bbe3b78d43113da57a71d1ace25e29c0
EmsDz <Ediaz@intellisys.com.do>
2022-02-17 15:11:14 -0400

    Implement:
    - Mongo connection
    - Mongo schemas for user & exercise
    - Post method for add user
    - Get method for get all users

--- server.js
+++ server.js
@@ -6,16 +6,54 @@ require('dotenv').config();
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
+});
+
+let exerciseSessionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+});
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  log: [exerciseSessionSchema],
+});
+
+let Session = mongoose.model('Session', exerciseSessionSchema);
+let User = mongoose.model('User', userSchema);
+
+app.post('/api/users', (request, response) => {
+  let newUser = new User({ username: request.body.username });
+
+  newUser.save((error, savedUser) => {
+    if (!error) {
+      let responseObject = {};
+      responseObject['username'] = savedUser.username;
+      responseObject['_id'] = savedUser.id;
+      response.json(responseObject);
+    }
+  });
+});
+
+app.get('/api/users', (request, response) => {
+  User.find({}, (error, arrayOfUsers) => {
+    if (!error) {
+      response.json(arrayOfUsers);
+    }
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
